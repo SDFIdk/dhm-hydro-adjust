93271c012e5ce8340a01aea0480332133231a5ca
Peter Limkilde Svendsen <pelis@sdfe.dk>
2021-09-02 12:48:24 +0200

    Use intermediate in-memory raster in burn_line_z

--- hydroadjust/cli/burn_line_z.py
+++ hydroadjust/cli/burn_line_z.py
@@ -22,18 +22,27 @@ def main():
 
     lines_datasrc = ogr.Open(lines_path)
 
-    output_driver = gdal.GetDriverByName("GTiff")
-    output_raster_dataset = output_driver.CreateCopy(
-        output_raster_path,
+    # Create an intermediate, in-memory dataset that the lines will be burned
+    # into. This is done in order to prevent writing a premature output raster
+    # in case something goes wrong during the line rasterization.
+    intermediate_driver = gdal.GetDriverByName("MEM")
+    intermediate_raster_dataset = intermediate_driver.CreateCopy(
+        "temp", # the dataset has to have a name
         input_raster_dataset,
     )
 
+    # Burn the line layers into the temp raster
     for layer in lines_datasrc:
-        burn_lines(output_raster_dataset, layer)
-        logging.info(f"burned layer {layer.GetName()} into raster")
+        burn_lines(intermediate_raster_dataset, layer)
+        logging.info(f"burned layer {layer.GetName()} into temporary raster")
 
-    output_raster_dataset = None
-    logging.info("output raster completed")
+    # Line rasterization is now complete, copy the temp raster to output file
+    output_driver = gdal.GetDriverByName("GTiff")
+    intermediate_raster_dataset = output_driver.CreateCopy(
+        output_raster_path,
+        intermediate_raster_dataset,
+    )
+    logging.info("output raster written")
 
 # Allows executing this module with "python -m"
 if __name__ == '__main__':
